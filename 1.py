35305b8234c31971a10b3e351bafa31b3f7a0931
kawuwa <3066839538@qq.com>
2022-10-30 12:39:31 +0800

    10/30/12:39

--- 1.py
+++ 1.py
@@ -12,6 +12,7 @@ from selenium import webdriver
 import time
 import jieba
 from wordcloud import WordCloud
+from imageio import imread
 
 hrefs=[]
 #<span class="total-box" style="">共 11 条 当前显示1-10条</span>
@@ -19,7 +20,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 browser=webdriver.Chrome(options=chrome_options)
 #browser = webdriver.Chrome()
-url = 'http://www.cninfo.com.cn/new/fulltextSearch?notautosubmit=&keyWord=中泰证券'
+url = 'http://www.cninfo.com.cn/new/fulltextSearch?notautosubmit=&keyWord=飞机'
 browser.get(url)
 
 data = browser.page_source
@@ -27,7 +28,7 @@ pnumber='<span class="total-box" style="">共.*?(\d+).*?条.*?当前'
 number=re.findall(pnumber,data)
 #pages=int(eval(number[0])/10)+1
 
-for i in range(60):
+for i in range(70):
     time.sleep(3)
     
     data = browser.page_source
@@ -41,30 +42,32 @@ for i in range(60):
     href = re.findall(p_href, data)
     date = re.findall(p_date, data, re.S)
     #print(number[0])
-    print(title)
+    #print(title)
     
     
     for j in range(len(title)):
-        title[j] = re.sub(r'<.*?>', '', title[j])
+        
+        title[j]=re.sub('<.?em>','',title[j])
         href[j] = 'http://www.cninfo.com.cn' + href[j]
         href[j] = re.sub('amp;', '', href[j])
         date[j] = date[j].strip()  # 清除空格和换行符
         date[j] = date[j].split(' ')[0]  # 只取“年月日”信息，不用“时分秒”信息
-        if '公告' not in title[j]:
-            title[j]=''
-            href[j]=''
-            date[j]=''
-            
-        print(str(j + 1) + '.' + title[j] + ' - ' + date[j])
-        print(href[j])
         
-    while '' in title:
-        title.remove('')
-    while '' in href:
-        href.remove('')
-    while '' in date:
-        date.remove('')
-    
+    #     if '腾讯控股' not in title[j]:
+    #         title[j]=''
+    #         href[j]=''
+    #         date[j]=''
+            
+    #     print(str(j + 1) + '.' + title[j] + ' - ' + date[j])
+    #     print(href[j])
+        
+    # while '' in title:
+    #     title.remove('')
+    # while '' in href:
+    #     href.remove('')
+    # while '' in date:
+    #     date.remove('')
+    print(title)
     print('第{}页成功'.format(i+1))
     
     for m in range(len(href)):
@@ -77,7 +80,8 @@ for i in range(60):
     #InvalidArgumentException: invalid报错原因是网址格式错误
         
     browser.find_element("css selector",'i.el-icon-arrow-right').click()
-    
+
+print(len(hrefs))
 browser.quit()
 
 chrome_options = webdriver.ChromeOptions()
@@ -110,7 +114,7 @@ filedir=r'D:\学校课程\大数据分析及应用\第10章源代码汇总\第10
 fileslist=[]
 for files in os.walk(filedir):
     for file in files[2]:
-        print(file)
+        
         if os.path.splitext(file)[1]=='.pdf' or os.path.splitext(file)[1]=='.PDF':
             fileslist.append(filedir+'\\'+file)
 #获取文件列表
@@ -135,6 +139,8 @@ tabletext=''
 for table in tableall:
     for rc in table:
         for cc in rc:
+            if not cc:
+                continue
             tabletext+=cc
 tabletext=re.sub('\n','',tabletext)
 alltext=textall+tabletext
@@ -143,13 +149,51 @@ alltext=textall+tabletext
 
 # print(textall)
 # print(tabletext)
-print(alltext)
 
-words = jieba.lcut(alltext)                                        #汉字精确分词
-newtxt = ' '.join(words)                                       #空格拼接中文词
-wordcloud = WordCloud(font_path=r"C:\Windows\Fonts\FZSTK.TTF").generate(newtxt)   #生成词云，font_path="msyh.ttc"为选择微软雅黑字体
+for ch in '。；，：」「“”（）、？《》!"#$%&()*+,-./:;<=>?@`[\\]^_‘{|}~\'0123456789':
+    alltext=alltext.replace(ch," ")
+alltext=alltext.replace('\u3000'," ")
+alltext=alltext.replace('\n'," ")
+words = jieba.lcut(alltext)                                #汉字精确分词
+
+
+smtext=set(words)
+
+dictw={}
+for h in smtext:
+    #print(words.count(h))
+    dictw[h]=words.count(h)
+lsdic=list(dictw.items())
+
+lsdic.sort(key = lambda x:x[1], reverse=True)
+
+qian50=[]
+d=lsdic[0][1]
+count=1
+for l in lsdic:
+    if l[1]<d:
+        count+=1
+    if count>51:
+        break
+    qian50.append(l)
+print(qian50[1:])
+print(len(qian50))
+
+lisnew=[]
+for g in qian50[1:]:
+    for k in range(g[1]):
+        lisnew.append(g[0])
+
+newtxt = ' '.join(lisnew)                                      #空格拼接中文词
+
+bg_pic = imread('plane.png')
+
+wordcloud = WordCloud(background_color='white',max_words=50,mask=bg_pic,font_path=r"C:\Windows\Fonts\SIMKAI.TTF").generate(' '.join(words))   #生成词云，font_path="msyh.ttc"为选择微软雅黑字体
+
 image = wordcloud.to_image()
-display(image)                              #显示词云图
-#wordcloud.to_file('pdfanalyze.png')	# 保存图片
+#display(image)
+wordcloud.to_file('pdfanalyze.png')	# 保存图片
+    
+
 
     
--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
-Git is a distributed version control system.
-Git is free software distributed under the GPL.
-Git has a mutable index called stage.
-Git tracks changes
-Creating a new branch is quick.
+2022年
+山东大学
+李建文
+大数据分析与应用
+期中测验
